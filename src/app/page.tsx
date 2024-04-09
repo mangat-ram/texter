8a27575a52fef6b6f73ec1154b68c05cebd9d1d8
mangat-ram <se.mangat.ram@gmail.com>
2024-04-09 21:55:43 +0530

    Added Shadcn UI .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 
 export default function Home() {
   return (
@@ -26,7 +27,7 @@ export default function Home() {
         href="/dashboard"
         target="_blank"
       >
-        Get Started
+        Get Started<ArrowRight className="ml-2 h-5 w-5" />
       </Link>
     </MaxWidthWrapper>
   );
